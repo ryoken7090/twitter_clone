18a643acb99fc94bdc01469f9b8acadcf0e73bbb
ryoken nakayama <ryoken7090@gmail.com>
2020-05-22 10:43:20 +0900

    indexのアクションとビューを作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,13 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
   def create
-      Message.create(message_params)
-    end
-private
+    Message.create(message_params)
+    redirect_to messages_path
+  end
+  def index
+    @messages = Message.all
+  end
+  private
   def message_params
     params.require(:message).permit(:content)
   end
