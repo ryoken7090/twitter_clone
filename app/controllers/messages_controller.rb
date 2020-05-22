c3ee01145f14157c32edc2aed767d0a1521b2679
ryoken nakayama <ryoken7090@gmail.com>
2020-05-22 10:28:16 +0900

    createアクションとparamsメソッドの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,3 +2,11 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
+  def create
+      Message.create(message_params)
+    end
+private
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
