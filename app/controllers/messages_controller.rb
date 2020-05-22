3036ffe60a9368f75ea01685c0009f3129c395db
ryoken nakayama <ryoken7090@gmail.com>
2020-05-22 10:54:48 +0900

    showアクションとビューを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,9 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+  def show
+    @message = Message.find(params[:id])
+  end
   private
   def message_params
     params.require(:message).permit(:content)
